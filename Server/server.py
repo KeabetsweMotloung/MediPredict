81081e1791b4aaee1a11d47ceda8e3f6e0d9242e
kemotlo022 <kemotlo022@student.wethinkcode.co.za>
2024-07-14 13:30:06 +0200

    Code works as expected and final touches will be added

--- Server/server.py
+++ Server/server.py
@@ -1,42 +1,49 @@
 import os
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 import joblib
 import numpy as np
-from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-model_path = os.path.join(os.path.dirname(__file__),'..', 'Model', 'Medical_model_gradientR.joblib')
+model_path = os.path.join(os.path.dirname(__file__), '..', 'Model', 'Medical_model_gradientR.joblib')
 
 medical_model = joblib.load(model_path)
 
-
 @app.route('/')
 def LandingPage():
-    return "Welcome to the MediPredict"
+    return render_template('MediPredict.html')
 
+@app.route('/predict_page')
+def predict_page():
+    return render_template('PredictPage.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-   # Get user input from POST request
-    input_data = request.get_json()
+    try:
+        # Extract features from POST request
+        age = int(request.form['age'])
+        sex = 1 if request.form['sex'].lower() == 'female' else 0
+        bmi = float(request.form['bmi'])
+        children = int(request.form['children'])
+        smoker = 1 if request.form['smoking_status'].lower() == 'yes' else 0
+        region = request.form['region'].lower()
 
-    # Extract features from user input
-    age = input_data['age']
-    sex = 1 if input_data['sex'].lower() == 'female' else 0
-    bmi = input_data['bmi']
-    children = input_data['children']
-    smoker = 1 if input_data['smoker'].lower() == 'yes' else 0
-    region = input_data['region']
+        # Encoding region
+        region_mapping = {'southwest': 1, 'southeast': 2, 'northwest': 3, 'northeast': 4}
+        region_encoded = region_mapping.get(region, 0)
 
-    client_data = np.array([[age, sex, bmi, children, smoker, region]])
+        client_data = np.array([[age, sex, bmi, children, smoker, region_encoded]])
 
-    # Make prediction using the loaded model
-    predicted_cost = medical_model.predict(client_data)
+        # Make prediction using the loaded model
+        predicted_cost = medical_model.predict(client_data)[0]
 
-    # Return the prediction as JSON response
-    return jsonify({'predicted_cost': predicted_cost[0]})
+        # Return the prediction as a new template
+        return render_template('PredictionResult.html', predicted_cost=predicted_cost)
+    
+    except KeyError as e:
+        return f"Missing form field: {e}", 400
+    except ValueError as e:
+        return f"Value error: {e}", 400
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
-
